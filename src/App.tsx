e9a21358f55b339f6a1238bea90078df66ead682
asm-saim <saim777m@gmail.com>
2026-08-31 20:23:29 +0600

    Different ypes of css implemented in react

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,26 @@ function App() {
   return (
     <>
       <h1>Hello there</h1>
-      <Calculator></Calculator>
+      <Student></Student>
+      <Student></Student>
+      <Student></Student>
     </>
+  );
+}
+
+//Components:
+function Student() {
+  const student = {
+    border: "2px solid green",
+    padding: "5px",
+    margin: "10px",
+    borderRadius: "5px",
+  };
+  return (
+    <div className="student">
+      <h1>Name</h1>
+      <h1>Roll:</h1>
+    </div>
   );
 }
 
