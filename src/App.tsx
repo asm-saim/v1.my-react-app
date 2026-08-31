3d728fb9bfe3c391b42467c04a3de76976ac18a0
asm-saim <saim777m@gmail.com>
2026-08-31 21:28:44 +0600

    props implemented

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,25 @@ function App() {
   return (
     <>
       <h1>Hello there</h1>
-      <Student></Student>
-      <Student></Student>
-      <Student></Student>
+      <Student name="Yamen" roll="35"></Student>
+      <Student name="Mastan" roll="47"></Student>
+      <Student name="Linkon" roll="32"></Student>
     </>
   );
 }
 
 //Components:
-function Student() {
-  const student = {
-    border: "2px solid green",
-    padding: "5px",
-    margin: "10px",
-    borderRadius: "5px",
-  };
+function Student(props) {
+  // const student = {
+  //   border: "2px solid green",
+  //   padding: "5px",
+  //   margin: "10px",
+  //   borderRadius: "5px",
+  // };
   return (
     <div className="student">
-      <h1>Name</h1>
-      <h1>Roll:</h1>
+      <h1>Name: {props.name}</h1>
+      <h1>Roll: {props.roll}</h1>
     </div>
   );
 }
