30185c47398f850e6bdc1d97173654bc55d898a3
asm-saim <saim777m@gmail.com>
2026-09-01 06:57:08 +0600

    props type declared and recived directly as object

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,6 +1,9 @@
-function Todo(props) {
-  console.log(props);
-  const { task, time } = props;
+interface TodoProps {
+  task: string;
+  time: string;
+}
+
+function Todo({ task, time }: TodoProps) {
   return (
     <div>
       <h4>Current Tasks remaining: {task}</h4>
@@ -8,5 +11,15 @@ function Todo(props) {
     </div>
   );
 }
+// function Todo(props:TodoProps) {
+//   console.log(props);
+//   const { task, time } = props;
+//   return (
+//     <div>
+//       <h4>Current Tasks remaining: {task}</h4>
+//       <h5>Time Duration : {time}</h5>
+//     </div>
+//   );
+// }
 
 export default Todo;
